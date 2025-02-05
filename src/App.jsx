998908cd94b71deaea9c9b5d8054080594a52af6
atikurahad <atikurahad@gmail.com>
2025-02-05 10:48:01 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import RegistrationPage from './pages/Registration/RegistrationPage'
+
+function App() {
+
+  return (
+    <>
+
+<RegistrationPag/>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/routes/routes.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+
+const routes = createBrowserRouter([
+
+    <BrowserRouter>
+    <Routes>
+      <Route path="/Registration" element={<App/>} />
+    </Routes>
+  </BrowserRouter>
+
+])
